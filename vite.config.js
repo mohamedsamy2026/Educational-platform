066bea6f3a694574762ceb32cd62cfc3f949538c
mohamed samy <mohamedeng747@gmil.com>
2026-09-09 01:21:57 +0300

    mohamed

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
