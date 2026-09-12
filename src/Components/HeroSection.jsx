b31e797edcb390454700873e0c152269abb35d80
mohamed samy <mohamedeng747@gmil.com>
2026-09-13 00:46:13 +0300

    mohamed

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,73 +1,106 @@
-// Import IMG
+import MasterImg from "../assets/Master/master-home.png";
 import HeroImg from "../assets/Background/hero-bg.webp";
-import MasterImg from "../assets/Master/Master transparent.png";
 
 export default function HeroSection() {
   return (
-    <section className="relative min-h-screen flex items-center overflow-hidden ">
-      {/* الخلفية التاريخية */}
+    <section
+      dir="rtl"
+      className="relative min-h-screen flex items-center overflow-hidden"
+    >
+      {/* Background Start */}
       <div className="absolute inset-0 z-0">
-        <img
-          src={HeroImg}
-          alt="خلفية تاريخية"
-          className="w-full h-full object-cover"
-        />
-        {/* Overlay غامق عشان النص يبان */}
-        <div className="absolute inset-0 bg-gradient-to-l from-midnight via-midnight/90 to-midnight/70"></div>
-      </div>
+     <img
+  src={HeroImg}
+  alt="خلفية تاريخية"
+  className="w-full h-full object-cover object-[50%_center] lg:object-center"
+/>
 
-      {/* المحتوى */}
-      <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-32">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
-          {/* النص (على اليمين في RTL) */}
-          <div className="text-right">
-            <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-warm-white mb-6 leading-tight">
+        {/* Dark Overlay */}
+        <div className="absolute inset-0 bg-black/45"></div>
+
+        {/* Blue Dark Overlay */}
+        <div className="absolute inset-0 bg-gradient-to-l from-[#061522]/75 via-[#071b2b]/35 to-black/40"></div>
+
+        {/* Bottom Dark Gradient */}
+        <div className="absolute inset-x-0 bottom-0 h-64 bg-gradient-to-t from-[#030b12] via-[#030b12]/60 to-transparent"></div>
+      </div>
+      {/*  Background End  */}
+
+      {/*  Content Start  */}
+      <div className="relative z-10 w-full max-w-7xl mx-auto px-5 sm:px-8 lg:px-10 py-32">
+        <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-8 lg:gap-4">
+          {/*  Text Start  */}
+          <div className="order-1 lg:order-2 text-right">
+            <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-7xl xl:text-7xl font-bold text-white leading-tight mb-6">
               تعلم التاريخ
               <br />
               <span className="text-gold">بأسلوب مختلف</span>
             </h1>
 
-            <p className="text-lg md:text-xl text-white mb-8 leading-relaxed max-w-2xl">
-              تعلم التاريخ مع مستر محمد خالد - أفضل مدرس دراسات اجتماعية للصفوف
-              الإعدادية والثانوية بأسلوب مبسط وشيق يجعلك تحب التاريخ وتفهمه بعمق
+            <p className="text-base sm:text-lg md:text-xl text-white leading-relaxed max-w-2xl mb-8 opacity-95">
+              تعلم التاريخ مع مستر محمد خالد - بخبرة تمتد لأكثر من 3 سنوات في
+              التدريس، نقدم لك أفضل أسلوب لشرح الدراسات الاجتماعية للصفوف
+              الإعدادية والثانوية بطريقة مبسطة وشيقة تجعلك تحب التاريخ وتفهمه
+              بعمق.
             </p>
 
-            {/* الأزرار */}
-            <div className="flex flex-col sm:flex-row gap-4 justify-center mt-10">
+            {/* ================= Buttons ================= */}
+
+            <div className="flex flex-col w-full sm:flex-row gap-4 justify-start items-start">
               <a
                 href="/signup"
-                className="bg-gold text-gray-100 px-8 py-4 rounded-lg font-bold text-lg hover:bg-gold-light transition-all duration-200 shadow-lg hover:shadow-gold/50"
+                className="bg-gold text-white px-8 py-4 rounded-lg font-bold text-lg shadow-lg transition-all duration-300hover:bg-gold-light sm:w-auto w-full"
               >
                 ابدأ التعلم
               </a>
+
               <a
                 href="/courses"
-                className="border-2 border-gold text-gold px-8 py-4 rounded-lg font-bold text-lg hover:bg-gold hover:text-midnight transition-all duration-200"
+                className="border-2 border-gold text-gold px-8 py-4 rounded-lg font-bold text-lg transition-all duration-300 hover:bg-gold hover:text-white sm:w-auto w-full"
               >
                 شاهد الكورسات
               </a>
             </div>
           </div>
 
-          {/* صورة المدرس (على اليسار في RTL) */}
-          <div className="hidden lg:flex justify-center">
-            <div className="relative">
-              {/* تأثير الإضاءة الخلفية */}
-              <div className="absolute inset-0 bg-gold/20 blur-3xl rounded-full"></div>
+          {/* ================= Text End ================= */}
+
+          {/* ================= Master Start ================= */}
+
+          <div className="order-1 lg:order-2 relative flex items-center justify-center min-h-[450px] lg:min-h-[650px]">
+            {/* Person */}
+
+            <div className="relative w-full max-w-xl lg:min-w-3xl flex justify-center items-start">
               <img
                 src={MasterImg}
                 alt="مستر محمد خالد"
-                className="relative w-full max-w-xl h-auto object-contain drop-shadow-2xl"
+                className="
+      relative
+      z-10
+      w-full
+      h-auto
+      object-contain
+      object-bottom
+      drop-shadow-2xl
+      lg:scale-130
+      scale-120
+      lg:translate-y-6
+    "
+         style={{
+  WebkitMaskImage:
+    "linear-gradient(to bottom, black 88%, transparent 100%)",
+  maskImage:
+    "linear-gradient(to bottom, black 85%, transparent 100%)",
+}}
               />
-              <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-4/5 h-12 bg-black/4عععع0 blur-2xl rounded-full pointer-events-none"></div>
-              <div className="absolute  bottom-0 left-0 right-0 bg-black/20 blur-3xl rounded-full"></div>
             </div>
           </div>
+
+          {/* ================= Master End ================= */}
         </div>
       </div>
 
-      {/* تأثير ذهبي في الأسفل */}
-      <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-midnight to-transparent"></div>
+      {/* ================= Content End ================= */}
     </section>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -20,11 +20,12 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-midnight/80 backdrop-blur-md border-b border-border-navy">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    // Nav Links Start
+    <nav className="fixed top-0 left-0 right-0 z-100 backdrop-blur-sm bg-black/15">
+      <div className="max-w-7xl mx-auto  sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-22">
-          {/* اللوجو */}
-          <div className="flex-shrink-0 flex items-center cursor-pointer">
+          {/* Logo Start */}
+          <div className="flex-shrink-0 flex items-center cursor-pointer mx-7">
             <img
               src={logoImg}
               alt="الغازي في التاريخ"
@@ -33,9 +34,10 @@ export default function Navbar() {
               }}
             />
           </div>
+          {/* Logo End */}
 
-          {/* روابط التنقل (Desktop) */}
-          <div className="hidden md:flex items-center gap-10">
+          {/* Links Desktop Start*/}
+          <div className="hidden lg:flex items-center gap-10">
             {navLinks.map((link) => (
               <a
                 key={link.name}
@@ -46,22 +48,31 @@ export default function Navbar() {
               </a>
             ))}
           </div>
+          {/* Links Desktop End*/}
 
-          {/* زر تسجيل الدخول */}
-          <div className="hidden md:flex items-center">
+          {/* LoigIN & SignUp Start */}
+          <div className="hidden lg:flex items-center gap-5">
             <a
               href="/login"
-              className="bg-yellow-500 text-white px-6 py-3 rounded-lg font-bold hover:bg-yellow-600 transition-colors duration-200"
+              className="bg-[#D4AF37] text-white px-6 py-3 rounded-lg font-bold hover:bg-[#B8941F] transition-colors duration-200"
             >
               تسجيل الدخول
             </a>
-          </div>
 
-          {/* زر القائمة (Mobile) */}
-          <div className="md:hidden mx-7">
+            <a
+              href="/login"
+              className="bg-[#D4AF37] text-white px-6 py-3 rounded-lg font-bold hover:bg-[#B8941F] transition-colors duration-200"
+            >
+              انشاء حساب
+            </a>
+          </div>
+          {/* LoigIN & SignUp End */}
+
+          {/* Button(Mobile) Start */}
+          <div className="lg:hidden mx-7">
             <button
               onClick={() => setIsOpen(!isOpen)}
-              className="text-warm-white hover:text-gold focus:outline-none text-2xl"
+              className="text-warm-white hover:text-gold focus:outline-none text-3xl"
             >
               {isOpen ? (
                 <FontAwesomeIcon className="cursor-pointer" icon={faXmark} />
@@ -70,13 +81,14 @@ export default function Navbar() {
               )}
             </button>
           </div>
+          {/* Button(Mobile) End */}
         </div>
 
-        {/* قائمة الموبايل */}
+        {/* Navbar Mobiles Start*/}
         <div
-          className={`md:hidden bg-charcoal border-t border-border-navy transition-all duration-300 ease-in-out ${
+          className={`md:hidden bg-gradient-to-b backdrop-blur-sm bg-black/25 border-t-1 border-white transition-all duration-300 ease-in-out ${
             isOpen
-              ? "max-h-96 opacity-100 pb-5 pt-3 pointer-events-auto"
+              ? "max-h-auto opacity-100 pb-5 pt-3 pointer-events-auto"
               : "max-h-0 opacity-0 py-0 pointer-events-none"
           }`}
         >
@@ -84,19 +96,27 @@ export default function Navbar() {
             <a
               key={link.name}
               href={link.href}
-              className="block px-4 py-[17px] text-warm-white font-bold hover:text-gold hover:bg-navy/50 transition-colors"
+              className="block px-4 py-[17px] text-warm-white font-bold hover:text-gold hover:bg-black/25 transition-colors"
             >
               {link.name}
             </a>
           ))}
           <a
             href="/login"
-            className="block mx-4 mt-4 bg-yellow-500 text-white font-bold px-6 py-3 rounded-lg text-center hover:bg-yellow-600 transition-colors"
+            className="block mx-4 mb-6 mt-4 bg-[#D4AF37] text-white font-bold px-6 py-3 rounded-lg text-center hover:bg-[#B8941F] transition-colors"
           >
             تسجيل الدخول
           </a>
+          <a
+            href="/login"
+            className="block mx-4 mt-4 bg-[#D4AF37] text-white font-bold px-6 py-3 rounded-lg text-center hover:bg-[#B8941F] transition-colors"
+          >
+            انشاء حساب
+          </a>
         </div>
+        {/* Navbar Mobiles End*/}
       </div>
     </nav>
+    // Nav Links End
   );
 }
